21d8f3790d0e19aafbc66d449fc1fad4e92f12f0
Manoj Panta <manojpanta95@gmail.com>
2018-05-21 22:27:32 -0600

    added delete functionality for user ideas

---
+++ spec/features/users/user_can_create_their_ideas_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'when user visits user home  page' do
+  scenario 'a user can log in and create a new idea' do
+    username = 'manoj'
+    password = '1234'
+    content = 'This is my idea'
+    title = 'idea1'
+    category = Category.create!(name: 'Information')
+    category1 = Category.create!(name: 'Information1')
+    category2 = Category.create!(name: 'Information2')
+    user = User.create(username: username, password: password)
+
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'username', with: username
+    fill_in 'password', with: password
+    click_on 'Log In'
+
+    expect(current_path).to eq(user_path(user))
+
+    click_on 'Create New Idea'
+
+    expect(current_path).to eq(new_user_idea_path(user))
+
+    fill_in 'idea[content]', with: content
+    fill_in 'idea[title]', with: title
+    select 'Information', from: 'idea_category_id'
+    click_on 'Create Idea'
+
+    expect(current_path).to eq(user_idea_path(user, Idea.last))
+    expect(page).to have_content(title)
+    expect(page).to have_content(content)
+  end
+end
---
+++ spec/features/users/user_can_see_all_their_ideas_in_their_show_page_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+describe 'when user visits user show page' do
+  scenario 'a user can see all their ideas' do
+    username = 'manoj'
+    password = '1234'
+    content = 'This is my idea'
+    title = 'idea1'
+    category = Category.create!(name: 'Information')
+    category1 = Category.create!(name: 'Information1')
+    category2 = Category.create!(name: 'Information2')
+
+    user = User.create(username: username, password: password)
+    idea1 = Idea.create(user: user, title: 'walking', content: 'walk slowly', category: category)
+    idea2 = Idea.create(user: user, title: 'running', content: 'run slowly', category: category1)
+    idea3 = Idea.create(user: user, title: 'climbing', content: 'climb slowly', category: category2)
+    idea4 = Idea.create(user: user, title: 'jogging', content: 'jog slowly', category: category)
+
+
+
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'username', with: username
+    fill_in 'password', with: password
+    click_on 'Log In'
+
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_content(idea1.title)
+    expect(page).to have_content(idea2.title)
+    expect(page).to have_content(idea3.title)
+    expect(page).to have_content(idea4.title)
+  end
+  scenario 'user can go to specific idea by clicking on idea' do
+    username = 'manoj'
+    password = '1234'
+    content = 'This is my idea'
+    title = 'idea1'
+    category = Category.create!(name: 'Information')
+
+    user = User.create(username: username, password: password)
+    idea = Idea.create(user: user, title: 'walking', content: 'walk slowly', category: category)
+
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'username', with: username
+    fill_in 'password', with: password
+    click_on 'Log In'
+
+    click_on idea.title
+
+    expect(current_path).to eq(user_idea_path(user, idea))
+
+    expect(page).to have_content(idea.title)
+    expect(page).to have_content(idea.content)
+  end
+  scenario 'user can delete idea by clicking on delete next to idea' do
+    username = 'manoj'
+    password = '1234'
+    content = 'This is my idea'
+    title = 'idea1'
+    category = Category.create!(name: 'Information')
+
+    user = User.create(username: username, password: password)
+    idea = Idea.create(user: user, title: 'walking', content: 'walk slowly', category: category)
+
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'username', with: username
+    fill_in 'password', with: password
+    click_on 'Log In'
+
+    click_on 'Delete'
+
+    expect(current_path).to eq(user_ideas_path(user))
+
+    expect(page).to have_content("#{idea.title} Deleted!")
+  end
+end
