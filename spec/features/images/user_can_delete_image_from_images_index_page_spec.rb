55ee045736e21aeb592752a8caebde35ac929a14
Manoj Panta <manojpanta95@gmail.com>
2018-05-21 20:30:11 -0600

    added delete button for every images in images index page

---
+++ spec/features/images/user_can_delete_image_from_images_index_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'when admin visits images  page ' do
+  scenario ' they can delete image by clicking delete under the image' do
+    title = 'Image1'
+    url = 'https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg'
+
+    image = Image.create(title: title, url: url)
+
+    visit images_path
+    click_on 'Delete'
+
+    expect(current_path).to eq(images_path)
+
+    expect(page).to_not have_content(title)
+  end
+end
