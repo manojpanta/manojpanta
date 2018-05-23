1ec76953e2adcdb9f2774f215d3db089aba94bf6
Manoj Panta <manojpanta95@gmail.com>
2018-05-22 21:44:22 -0600

    removed last line from show page of an idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_ 05_22_235800) do
+ActiveRecord::Schema.define(version: 2018_05_22_235800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
