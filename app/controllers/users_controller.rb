95700936a059d1184cf1c5d698670d6aaffe6a90
manojpanta <manojpanta95@gmail.com>
2019-01-27 21:35:25 -0700

    added redirect after sending message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if ContactMeMailer.contact(user.name, user.email, user.message).deliver_now
       flash[:success] = 'Sent Successfully!!'
-      redirect_to root_path
 
     else
       flash[:failed] = 'Try Again'
       render :create
     end
+    redirect_to root_path
   end
 
   private
