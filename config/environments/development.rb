9188f29602aa75def406e07056c28e0e26f27790
manojpanta <manojpanta95@gmail.com>
2019-01-27 20:26:09 -0700

    added domain to smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
+    :domain => "https://manojpanta.herokuapp.com/",
     :user_name => ENV["USER_NAME"],
     :password => ENV["PASSWORD"],
     :authentication => "plain"
