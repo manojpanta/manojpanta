d700aa0cd73c614ab8bfc0c78eca7c4e292ae5d4
manojpanta <manojpanta95@gmail.com>
2019-02-08 01:21:31 -0700

    trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,10 +94,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method =  :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+  config.action_mailer.delivery_method =  :sendmail
+  config.action_mailer.sendmail_settings = {
+    :address => "sendmail.gmail.com",
     :port => 587,
     :user_name => ENV['USER_NAME'],
     :password => ENV['PASSWORD'],
