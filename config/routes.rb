5a8e858e3b97150bf7e07e42f80b55b5d67ebd74
manojpanta <manojpanta95@gmail.com>
2019-02-28 00:40:21 -0700

    added precompile and changed routes so looks cleaner

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   resources :resume, only: [:index]
   resources :users, only: [:create]
-  resources :games
-  post '/games/new', to: 'games#create'
+  get '/game',to: 'games#new'
+  post '/game', to: 'games#create'
   resources :contact, only: [:index]
   resources :mystory, only: [:index]
   resources :projects, only: [:index, :show]
