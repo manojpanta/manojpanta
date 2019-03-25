485d2b5cecbe0c353cd34eca5c122361c4937b27
manojpanta <manojpanta95@gmail.com>
2019-03-24 22:10:00 -0600

    added text to phone if some one contacts from website

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -4,10 +4,10 @@ class SendGridMailer
   include SendGrid
 
   def self.contact_me(name, email, message)
-    count = 0
-    until count == 10
-      from = Email.new(email: "manojpanta95#{count}@gmail.com")
-      subject = "You have new message from #{name} with email: #{email}"
+    # count = 0
+    # until count == 5
+      from = Email.new(email: "FromMyWebSite@gmail.com")
+      subject = "#{email}"
       to = Email.new(email: '7206832645@messaging.sprintpcs.com')
       content = Content.new(type: 'text/plain', value: "#{message}")
       mail = SendGrid::Mail.new(from, subject, to, content)
@@ -17,7 +17,18 @@ class SendGridMailer
       response.status_code
       response.body
       response.headers
-      count += 1
-    end
+
+      from = Email.new(email: "FromMyWebSite@gmail.com")
+      subject = "#{email}"
+      to = Email.new(email: 'manojpanta95@gmail.com')
+      content = Content.new(type: 'text/plain', value: "#{message}")
+      mail = SendGrid::Mail.new(from, subject, to, content)
+      mail.to_json
+      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+      response.status_code
+      response.body
+      response.headers
+      # count += 1
   end
 end
