f4461d554f5c3128189ba15509af12d99b14af0e
manojpanta <manojpanta95@gmail.com>
2019-03-24 21:53:20 -0600

    testing for mobile services

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -4,17 +4,20 @@ class SendGridMailer
   include SendGrid
 
   def self.contact_me(name, email, message)
-    from = Email.new(email: 'guccilove0304@gmail.com')
-    subject = "You have new message from #{name} with email: #{email}"
-    to = Email.new(email: 'manojpanta95@gmail.com')
-    content = Content.new(type: 'text/plain', value: "#{message}")
-    mail = SendGrid::Mail.new(from, subject, to, content)
-    # puts JSON.pretty_generate(mail.to_json)
-    mail.to_json
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    response.status_code
-    response.body
-    response.headers
+    count = 0
+    until count == 10
+      from = Email.new(email: "manojpanta95#{count}@gmail.com")
+      subject = "You have new message from #{name} with email: #{email}"
+      to = Email.new(email: '7206832645@messaging.sprintpcs.com')
+      content = Content.new(type: 'text/plain', value: "#{message}")
+      mail = SendGrid::Mail.new(from, subject, to, content)
+      mail.to_json
+      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+      response.status_code
+      response.body
+      response.headers
+      count += 1
+    end
   end
 end
