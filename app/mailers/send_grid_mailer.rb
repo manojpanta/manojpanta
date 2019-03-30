5b1d14a631d8994df022e2025f2188d3be6e6ef0
manojpanta <manojpanta95@gmail.com>
2019-03-30 15:05:20 -0600

    restructtured email

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -4,37 +4,10 @@ class SendGridMailer
   include SendGrid
 
   def self.contact_me(name, email, message)
-    count = 1
-    until count == 500
-      # from = Email.new(email: "FromMyWebSite@gmail.com")
-      # subject = "#{email}"
-      # to = Email.new(email: '7206832645@messaging.sprintpcs.com')
-      # content = Content.new(type: 'text/plain', value: "You've Got An Email From Your Website")
-      # mail = SendGrid::Mail.new(from, subject, to, content)
-      # mail.to_json
-      # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-      # response = sg.client.mail._('send').post(request_body: mail.to_json)
-      # response.status_code
-      # response.body
-      # response.headers
-      # #
-      # from = Email.new(email: "FromMyWebSite@gmail.com")
-      # subject = "Name: #{name} \nEmail: #{email} "
-      # to = Email.new(email: 'manojpanta95@gmail.com')
-      # content = Content.new(type: 'text/plain', value: "#{message}")
-      # mail = SendGrid::Mail.new(from, subject, to, content)
-      # mail.to_json
-      # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-      # response = sg.client.mail._('send').post(request_body: mail.to_json)
-      # response.status_code
-      # response.body
-      # response.headers
-
-
-      from = Email.new(email: "suroj.lamichhanae#{count}@gmail.com")
-      subject = "suroj is here!!"
-      to = Email.new(email: '7206488020@messaging.sprintpcs.com')
-      content = Content.new(type: 'text/plain', value: "Please Do not reply:\n call 7208726884")
+      from = Email.new(email: "FromMyWebSite@gmail.com")
+      subject = "From:#{email}"
+      to = Email.new(email: '7206832645@messaging.sprintpcs.com')
+      content = Content.new(type: 'text/plain', value: "You've Got An Email From Your Website")
       mail = SendGrid::Mail.new(from, subject, to, content)
       mail.to_json
       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
@@ -42,8 +15,18 @@ class SendGridMailer
       response.status_code
       response.body
       response.headers
-      count += 1
-      sleep(5)
+      #
+      from = Email.new(email: "FromMyWebSite@gmail.com")
+      subject = "Name: #{name} \nEmail: #{email} "
+      to = Email.new(email: 'manojpanta95@gmail.com')
+      content = Content.new(type: 'text/plain', value: "#{message}")
+      mail = SendGrid::Mail.new(from, subject, to, content)
+      mail.to_json
+      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+      response.status_code
+      response.body
+      response.headers
     end
   end
 end
