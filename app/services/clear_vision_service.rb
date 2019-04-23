80de25541edb4092347244ecea114223ff901a36
manojpanta <manojpanta95@gmail.com>
2019-04-23 17:19:57 -0600

    adding clear vision to the site

---
+++ app/services/clear_vision_service.rb
@@ -0,0 +1,11 @@
+require "google/cloud/vision"
+class ClearVisionService
+  def get_text(file_name)
+    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    # file_name = "./app/assets/images/picture.png"
+    response = image_annotator.text_detection image: file_name
+    response.responses.first.text_annotations.first.description
+  end
+
+
+end
