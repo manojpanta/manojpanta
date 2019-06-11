e8376bfd6bbb73e9cdf3f672c3ddf34eb0bbc2dd
manojpanta <manojpanta95@gmail.com>
2019-06-10 21:12:09 -0600

    use different credentials file for google cloud vision api

--- app/services/clear_vision_service.rb
+++ app/services/clear_vision_service.rb
@@ -1,4 +1,7 @@
+# require 'uri'
+# require 'image'
 require "google/cloud/vision"
+
 class ClearVisionService
   def get_text(file_name)
     image_annotator = Google::Cloud::Vision::ImageAnnotator.new
@@ -6,3 +9,38 @@ class ClearVisionService
     response.responses.first.text_annotations.first.description
   end
 end
+
+##using api key
+
+# binding.pry
+# api_key = ENV["GOOGLE-VISION-API"]
+# url = "https://vision.googleapis.com/v1/images:annotate?key=#{api_key}"
+# uri = URI.join('file:///', file_name)
+# uri.scheme
+# uri.path
+# uri.to_s
+#
+#
+# body = {
+#   "requests": [
+#     {
+#       "features": [
+#         {
+#           "type": "text_detection"
+#         }
+#       ],
+#       "image": {
+#         "source": {
+#           "imageUri": uri
+#         }
+#       }
+#     }
+#   ]
+# }
+# binding.pry
+# response = Faraday.post(url) do |req|
+#   req.headers["Content-Type"] = "application/json"
+#   req.body = body.to_json
+# end
+# binding.pry
+# response.body
